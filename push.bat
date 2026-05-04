deaddd8e2357af77facfdc239b34328e2870c4b1
thyids <gshiji@163.com>
2026-05-04 12:31:31 +0800

    更新代码

--- push.bat
+++ push.bat
@@ -1,14 +1,14 @@
 @echo off
 chcp 65001 >nul
 echo ==============================================
-echo          Git 强制提交 & 覆盖远程
+echo           Git 正常提交（只更新修改的文件）
 echo ==============================================
 echo.
 
 git add .
-git commit -m "强制更新代码"
-git push main main --force
+git commit -m "更新代码"
+git push main main
 
 echo.
-echo 已强制覆盖远程仓库！
+echo 提交成功！GitHub 已同步你修改的文件～
 pause
